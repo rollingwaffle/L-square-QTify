d38ed308045011e943a98ec240a7a1589e41a033
Roshan <roshanrazz30@gmail.com>
2024-06-28 08:45:09 +0530

    ToolTip implementation

--- qtify/src/components/card/card.jsx
+++ qtify/src/components/card/card.jsx
@@ -3,6 +3,7 @@ import img from "../../assets/Rectangle 2138 (1).png";
 import Chip from "@mui/material/Chip";
 import styles from "./card.module.css";
 import { StyledEngineProvider } from "@mui/material/styles";
+import Tooltip from "@mui/material/Tooltip";
 
 const CardComponent = ({ data, type }) => {
   const { title, image } = data;
@@ -13,21 +14,28 @@ const CardComponent = ({ data, type }) => {
   } else {
     followsOrLikes = data.likes;
   }
+
+  const generateRandomSongs = () => {
+    const num = Math.trunc(Math.random() * 100);
+    return `${num} songs`;
+  };
   // console.log("data", data, type);
   return (
     <StyledEngineProvider injectFirst>
-      <div className={styles.card}>
-        <img className={styles.cardImg} src={image} alt={title} />
-        <div className={styles.bottomCard}>
-          <Chip
-            label={`${followsOrLikes} ${
-              type === "album" ? "Follows" : "Likes"
-            }`}
-            className={styles.chip}
-          />
+      <Tooltip title={generateRandomSongs()} arrow>
+        <div className={styles.card}>
+          <img className={styles.cardImg} src={image} alt={title} />
+          <div className={styles.bottomCard}>
+            <Chip
+              label={`${followsOrLikes} ${
+                type === "album" ? "Follows" : "Likes"
+              }`}
+              className={styles.chip}
+            />
+          </div>
+          <span className={styles.cardText}>{title}</span>
         </div>
-        <span className={styles.cardText}>{title}</span>
-      </div>
+      </Tooltip>
     </StyledEngineProvider>
   );
 };
